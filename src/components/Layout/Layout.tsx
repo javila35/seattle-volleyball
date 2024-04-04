92141cf58ffedf520c8e0161e76f24dd1210958c
Joe Avila <josepharthuravila@gmail.com>
2024-04-03 21:46:23 -0700

    fix: decap config

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,22 +2,26 @@ import "../../styles/reset.css";
 import "../../styles/typography.css";
 import React, { PropsWithChildren } from "react";
 import { Content, Footer, Grid, Header, Nav, NavLink } from "./Layout.styles";
+import { Script, withPrefix } from "gatsby";
 
 type LayoutProps = {};
 
 export const Layout = ({ children }: PropsWithChildren<LayoutProps>) => {
   return (
-    <Grid>
-      <Header>Seattle Volleyball</Header>
-      <Nav>
-        <ul>
-          <NavLink>Tournaments</NavLink>
-          <NavLink>Leagues</NavLink>
-          <NavLink>Open Gyms</NavLink>
-        </ul>
-      </Nav>
-      <Content>{children}</Content>
-      <Footer>Footer</Footer>
-    </Grid>
+    <>
+      <Script src={withPrefix("netlifyIdentityRedirect.js")}></Script>
+      <Grid>
+        <Header>Seattle Volleyball</Header>
+        <Nav>
+          <ul>
+            <NavLink>Tournaments</NavLink>
+            <NavLink>Leagues</NavLink>
+            <NavLink>Open Gyms</NavLink>
+          </ul>
+        </Nav>
+        <Content>{children}</Content>
+        <Footer>Footer</Footer>
+      </Grid>
+    </>
   );
 };
